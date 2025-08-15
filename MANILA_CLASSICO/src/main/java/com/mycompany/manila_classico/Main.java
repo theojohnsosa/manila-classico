f1506be95fd95093680edaaf93fc91ebca6a87c0
theojohnsosa <theojohnsosa@gmail.com>
2025-08-15 12:27:41 +0800

    new initial commit

---
+++ MANILA_CLASSICO/src/main/java/com/mycompany/manila_classico/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manila_classico;
+
+/**
+ *
+ * @author Joana Sosa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.setLocationRelativeTo(null);
+        dashboardPage.setResizable(false);
+        dashboardPage.setVisible(true);
+    }
+}
